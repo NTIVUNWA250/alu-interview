8ba1814060e1aa7f79edd639af373239a46a00ba
NTIVUNWA250 <g.ntivunwa@alustudent.com>
2026-01-18 21:18:14 +0200

    Hey

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"A program to make a copy of n numbers of h"
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
+
